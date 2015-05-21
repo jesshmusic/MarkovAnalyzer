4fd1df28d2ad923c180d58cff09bfcbd5307f883
Jess Hendricks <jesshendricks1@Macbook-Pro.local>
2015-05-21 11:58:16 -0400

    Initial Commit

---
+++ MarkovAnalyzer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MarkovAnalyzer
+//
+//  Created by Fupduck Central MBP on 5/21/15.
+//  Copyright (c) 2015 Existential Music. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
